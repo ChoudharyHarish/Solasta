d970d0d1d178f04cd2214ce2a68f109e475ded6b
Sohaib Aftab <90454047+HattySohaib@users.noreply.github.com>
2024-01-27 07:46:19 -0800

    Add files via upload

--- client/src/components/Reasons.js
+++ client/src/components/Reasons.js
@@ -1,47 +1,21 @@
-import React from 'react'
+import React from "react";
 import Styles from "./Reason.module.scss";
-import SectionHeader from './SectionHeader';
-import {useNavigate} from "react-router-dom";
+import SectionHeader from "./SectionHeader";
 
-import contactbtn from "../assets/contactbtn.png";
 import data from "../constants/sponsor";
-
-
-
-
-const ServiceCard = ({ img, data }) => {
-    return (
-        <figure className={`${Styles['fig-container']} bg-white border-2 border-red-400`}>
-            <img src={img} alt="Service image" />
-            <figcaption>{data}</figcaption>
-        </figure>
-    )
-}
+import SponsorCard from "./SponsorCard";
 
 const Reason = () => {
-
-    const navigate = useNavigate();
-
-    return (
-        <section id="grid-section" className={Styles['service-section']} >
-            <SectionHeader
-                color="#f05"
-                heading="Reasons to Sponsor"
-                along="SOLASTA"
-            />
-
-            <div className={Styles['service-container']}>
-                {data.map((service, i) => <ServiceCard key={i} {...service} />)}
-            </div>
-
-
-            {/* <div className="img-btn">
-                <img draggable={false} src={contactbtn} alt="Button" onClick={() => navigate("/teams")} />
-            </div> */}
-
-        </section>
-    )
-}
-
+  return (
+    <section id="grid-section" className={Styles["service-section"]}>
+      <SectionHeader color="#f05" heading="Our" along="SPONSORS" />
+      <div className="sponsor-list">
+        {data.map((sponsor, i) => (
+          <SponsorCard key={i} {...sponsor} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Reason;
